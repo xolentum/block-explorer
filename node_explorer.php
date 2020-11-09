2896808bb945ee984f84525d34c02fbb03e8bb9a
boidushya <boidushyabhattacharya@gmail.com>
2020-11-09 21:25:11 +0530

    changed url and subsequent params

--- node_explorer.php
+++ node_explorer.php
@@ -37,11 +37,11 @@ function do_table($url,$name,$req_params){
 	$arr = json_decode($check_url,true);
 	$member = array(
 		"NODE_NAME"=>$name,
-		"HOSTNAME:PORT"=>$req_params["url"].":".$req_params["port"],
+		"HOSTNAME:PORT"=>$req_params["host"].":".$req_params["port"],
 		"VERSION"=>"1.0.0",
 		"HEIGHT"=>$arr["height"],
 		"IN/OUT (TX)"=>$arr["incoming_connections_count"]."/".$arr["outgoing_connections_count"]." ({$arr["tx_pool_size"]})",
-		"UPTIME"=>get_time_diff($req_params["start_time"])
+		"UPTIME"=>get_time_diff($arr["start_time"])
 	);
 	return $member;
 }
@@ -58,14 +58,14 @@ function get_time_diff($original){
 	return $interval->format('%aD %hH %iM %sS');
 }
 
-function get_nodes_table(){
-	$base_arr = json_decode(file_get_contents("https://raw.githubusercontent.com/xolentum/public-nodes-json/main/xolentum-public-nodes.json",true),true)["nodes"];
+function get_nodes_table($url){
+	$base_arr = json_decode(file_get_contents($url,true),true);
 	$final_arr = array();
 	foreach($base_arr as $req_params){
 		$badge = $req_params["ssl"] ? "<i class=\"fa fa-shield\" aria-hidden=\"true\"></i>" : "";
 		$NODE_NAME = $badge.$req_params["name"];
 		$ssl = $req_params["ssl"]? "s" : "";
-		$REQ_URL = "http".$ssl."://".$req_params["url"].":".$req_params["port"];
+		$REQ_URL = "http".$ssl."://".$req_params["host"].":".$req_params["port"];
 		$pushup = do_table($REQ_URL,$NODE_NAME,$req_params);
 		if($pushup!==null){
 			array_push($final_arr,$pushup);
@@ -76,6 +76,6 @@ function get_nodes_table(){
 }
 
 // unhash line if you want to print table
-// echo @get_nodes_table();
+// echo get_nodes_table("http://sohamb03.ml:1234/nodes");
 
 ?>
